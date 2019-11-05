64276d338bdeace82c1489ac0acbc83d92dbfca2
Mwandia <JEMwandia@gmail.com>
2019-11-05 13:34:18 +0300

    added breaks to control flow

--- app/src/main/java/local/test/processors/TextProcessor.java
+++ app/src/main/java/local/test/processors/TextProcessor.java
@@ -14,10 +14,10 @@ public class TextProcessor {
             String[] line = details[i].split(" ");
             for(int j = 0; j<line.length; j++) {
                 String word = line[j];
-                if (word.equals("NUMBER") && line[j].length() == 8) {
-                    id = Integer.parseInt(details[i+1]);
+                if (word.equals("NUMBER") && line[j+1].length() == 8) {
+                    id = Integer.parseInt(line[j+1]);
+                    break;
                 }
-                break;
             }
         }
 
@@ -32,6 +32,7 @@ public class TextProcessor {
             String line = details[i];
             if (line.equals("DATE OF BIRTH")) {
                 date = details[i + 1];
+                break;
             }
         }
 
@@ -45,6 +46,7 @@ public class TextProcessor {
             String line = details[i];
             if(line.equals("FULL NAMES")){
                 names = details[i+1];
+                break;
             }
         }
 
@@ -58,6 +60,7 @@ public class TextProcessor {
             String line = details[i];
             if(line.equals("SEX")){
                 sex = details[i+1];
+                break;
             }
         }
 
